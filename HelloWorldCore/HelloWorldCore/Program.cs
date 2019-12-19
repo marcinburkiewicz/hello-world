bc754af8753074b03383a38ee59e6af517a77855
Marcin.Burkiewicz <marcinburkiewicz@gmail.com>
2019-12-19 12:31:42 +0100

    Added comment into dev branch

--- HelloWorldCore/HelloWorldCore/Program.cs
+++ HelloWorldCore/HelloWorldCore/Program.cs
@@ -4,6 +4,7 @@ namespace HelloWorldCore {
 	class Program {
 		static void Main(string[] args) {
 			Console.WriteLine("Hello World from git directly from webPage!");
+			//Added something in dev
 		}
 	}
 }
