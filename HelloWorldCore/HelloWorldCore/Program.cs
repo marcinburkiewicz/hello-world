4e158f577163cfcdc6d94dc8a0c3aced1bee493e
Marcin.Burkiewicz <marcinburkiewicz@gmail.com>
2020-01-03 14:58:06 +0100

    Updgare tool to version 2

--- HelloWorldCore/HelloWorldCore/Program.cs
+++ HelloWorldCore/HelloWorldCore/Program.cs
@@ -4,6 +4,7 @@ namespace HelloWorldCore {
 	class Program {
 		static void Main(string[] args) {
 			Console.WriteLine("Hello World from git directly from webPage again oops!");
+			Console.WriteLine("This is version 2 of app");
 		}
 	}
 }
