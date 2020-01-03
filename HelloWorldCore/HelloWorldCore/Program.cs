308b1b5f07ea48a208f5927af4283dcbd4be23b5
Marcin.Burkiewicz <marcinburkiewicz@gmail.com>
2020-01-03 15:08:35 +0100

    Upgrade version to 2

--- HelloWorldCore/HelloWorldCore/Program.cs
+++ HelloWorldCore/HelloWorldCore/Program.cs
@@ -3,7 +3,8 @@
 namespace HelloWorldCore {
 	class Program {
 		static void Main(string[] args) {
-			Console.WriteLine("Hello World from git directly from webPage fixed");
+			Console.WriteLine("Hello World from git directly from webPage fixed v2 released");
+
 		}
 	}
 }
