bcfd46bc916ea71dd1250aa638227b41d31627e5
Marcin.Burkiewicz <marcinburkiewicz@gmail.com>
2020-02-28 13:11:18 +0100

    Hotfix made

--- HelloWorldCore/HelloWorldCore/Program.cs
+++ HelloWorldCore/HelloWorldCore/Program.cs
@@ -4,6 +4,7 @@ namespace HelloWorldCore {
 	class Program {
 		static void Main(string[] args) {
 			Console.WriteLine("Hello World from git directly from webPage fixed v2 released");
+			Console.Write("Error fix here");
 			Console.Write("");
 		}
 	}
