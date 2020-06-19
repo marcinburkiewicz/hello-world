805a55761093faf632b5a2d924167b0ed9c21bff
Marcin.Burkiewicz <Marcin.Burkiewicz@woodward.com>
2020-06-19 10:28:29 +0200

    Sec change

--- HelloWorldCore/HelloWorldCore/Program.cs
+++ HelloWorldCore/HelloWorldCore/Program.cs
@@ -5,6 +5,7 @@ namespace HelloWorldCore {
 		static void Main(string[] args) {
 			Console.WriteLine("Hello World merge");
 			Console.WriteLine("UnifyDb");
+			Console.WriteLine("UnifyDbSec Change");
 		}
 	}
 }
